ae9b4cc4ba05e752195357e45f493f88b1fd21f1
mollen <stepanpanenko@gmail.com>
2026-03-07 14:18:28 +0300

    TextFile.java constructor attributes init

--- src/main/java/fileControlSystem/TextFile.java
+++ src/main/java/fileControlSystem/TextFile.java
@@ -1,16 +1,21 @@
 package fileControlSystem;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static fileControlSystem.Attributes.PATH;
+
 class TextFile
 {
-    private final Map<String, String> attributes = Map.of();
+    private final Map<String, String> attributes;
     private final List<String> lines;
 
     public TextFile(File file)
     {
+        this.attributes = new HashMap<>();
+        attributes.put(PATH, file.getPath());
         this.lines = file.toString().lines().toList();
     }
 
