098fedf3f77def8b30681b07d0d998a456023a7e
mollen <stepanpanenko@gmail.com>
2026-03-07 14:16:43 +0300

    LetterImporter logic

--- src/main/java/fileControlSystem/LetterImporter.java
+++ src/main/java/fileControlSystem/LetterImporter.java
@@ -2,20 +2,23 @@ package fileControlSystem;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+
+import static fileControlSystem.Attributes.*;
 
 public class LetterImporter implements Importer
 {
+    static final String NAME_PREFIX = "Уважаемый ";
+
     @Override
     public Document importFile(File file) throws IOException
     {
-        final Map<String, String> attributes = new HashMap<>();
-        attributes.put(Attributes.PATH, file.getPath());
-        attributes.put(, )
+        TextFile textFile = new TextFile(file);
+        textFile.addLineSuffix(NAME_PREFIX, NAME);
+
+        final Map<String, String> attributes = new  java.util.HashMap<>(textFile.getAttributes());
+        attributes.put(TYPE, LETTER);
 
         return new Document(attributes);
     }
-
-
 }
