1c112a441793785580c08a1ecea206b1d14d0c30
mollen <stepanpanenko@gmail.com>
2026-03-10 14:05:39 +0300

    shouldImportReceiptAttribute() test

--- src/test/java/FileControlSystemTest.java
+++ src/test/java/FileControlSystemTest.java
@@ -19,6 +19,7 @@ public class FileControlSystemTest {
     private final String LETTER = RESOURCES + "patient.letter";
     private final String INVOICE = RESOURCES + "patient.invoice";
     private final String REPORT = RESOURCES + "patient.report";
+    private final String RECEIPT = RESOURCES + "patient.receipt";
     private final String IMAGE = RESOURCES + "xray.jpg";
 
     private final String JOE_BLOGGS = "Joe Bloggs";
@@ -79,6 +80,32 @@ public class FileControlSystemTest {
                         "We discussed his switch from drinking Coke to Diet Coke.\n" +
                         "No new problems were noted with his teeth.");
         assertTypeIs(Attributes.REPORT, document);
+    }
+
+    @Test
+    public void shouldImportReceiptAttribute() throws Exception {
+        system.importFile(RECEIPT);
+
+        final Document document = onlyDocument();
+
+        assertAttributeEquals(document, Attributes.PATH, RECEIPT);
+        assertAttributeEquals(document, NAME, JOE_BLOGGS);
+        assertAttributeEquals(document, Attributes.ADDRESS,
+                "123 Fake Street\n" +
+                        "Westminster\n" +
+                        "London\n" +
+                        "United Kingdom");
+        assertAttributeEquals(document, Attributes.MEDICINAL_PRODUCT, "antibiotic");
+        assertAttributeEquals(document, Attributes.DOSAGE_AND_SCHEDULE,
+                "take as prescribed: follow the exact dosage and frequency instructed by your doctor. Do not take more or less than the specified amount.\n" +
+        "Maintain consistency: To ensure a steady level of the medication in your bloodstream, take the antibiotic at regular intervals throughout the day\n" +
+        "Timing: If instructed to take the medicine multiple times daily, spread the doses out evenly (e.g., every 8 or 12 hours).\n" +
+        "Administration Conditions: With or without food: Some antibiotics should be taken on an empty stomach (typically 1 hour before or 2 hours after a meal) for optimal absorption,\n" +
+        "while others are best taken with food to reduce potential stomach upset.\n" +
+        "Hydration: Take the medication with a full glass of water.\n" +
+        "Avoid taking it with dairy products, certain juices, or alcohol, as these can interfere with the drug's effectivenessHydration: Take the medication with a full glass of water.\n" +
+        "Avoid taking it with dairy products, certain juices, or alcohol, as these can interfere with the drug's effectiveness.");
+        assertTypeIs(Attributes.RECEIPT, document);
     }
 
     @Test
